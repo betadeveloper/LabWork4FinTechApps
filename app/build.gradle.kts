d52edafc8f4211aa126acebbcbed384d10e5988b
Raigardas Tautkus <betadeveloper@users.noreply.github.com>
2026-04-02 12:20:59 +0300

    Update gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(libs.junit)
+    testImplementation("org.robolectric:robolectric:4.11.1")
+    testImplementation("org.mockito:mockito-core:5.7.0")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
